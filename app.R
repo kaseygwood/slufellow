1b6e92b64c08741b258d3e8b9912ecdd6bbe9214
Kasey Wood <kgwood20@stlawu.edu>
2023-06-07 16:45:39 -0400

    Edits on the app to add two more tabs and graphs. Still issues with these, two of the tabs don't work after the other has been clicked. Tab added to track enrollment yearly and tab added to track enrollment yearly with two lines for the different semesters.

--- app.R
+++ app.R
@@ -1,20 +1,79 @@
 library(shiny)
-
+library(ggthemes)
 ui <- fluidPage(
-  # plot enrollment by semester
-  selectInput("course", label = "Select a Course", choices = course_reg_data$`Course Title`),
-  plotOutput(outputId = "plot")
-)
+  theme = bslib::bs_theme(bootswatch = "sandstone"),
+  tabsetPanel(
+    tabPanel("Enrollment by Course",
+             sidebarLayout(
+               sidebarPanel(
+                 tabsetPanel(
+                   tabPanel("Yearly",
+                            selectInput("courseYearly", label = "Select a Course", 
+                                        choices = course_reg_data_yearly$`Course Title`,
+                                        selected = course_reg_data_yearly$`Course Title`[1])),
+                   tabPanel("By Semester",
+                            selectInput("courseSemester", label = "Select a Course", 
+                                        choices = course_reg_data$`Course Title`, 
+                                        selected = course_reg_data$`Course Title`[1])),
+                   tabPanel("Compare Semesters",
+                            selectInput("course", label = "Select a Course", 
+                                        choices = course_reg_data$`Course Title`, 
+                                        selected = course_reg_data_fall$`Course Title`[1]))
+                            )),
+               mainPanel(
+                 plotlyOutput(outputId = "plot")
+                 )
+             )
+             )
+  ))
+
 
 server <- function(input, output, session) {
   # plot enrollment by semester
-  output$plot <- renderPlot({
-    course_app <- course_reg_data |> filter(`Course Title` %in% input$course)
-    ggplot(data = course_app, aes(x = `Acad Year Term`, y = Enrolled, group = 1)) +
-      geom_line() +
-      geom_point() +
-      labs(title = glue::glue("Enrollment for ", input$course))
+  output$plot <- renderPlotly({
+    if (!is.null(input$courseSemester) && input$courseSemester %in% course_reg_data$`Course Title`){
+      course_app_semester <- course_reg_data |> filter(`Course Title` %in% input$courseSemester) |> 
+        arrange(`Reporting Year`)
+      plot1 <- ggplot(data = course_app_semester, aes(x = `Acad Year Term`, y = Enrolled, group = 1)) +
+        geom_line() +
+        geom_point() +
+        labs(title = glue::glue("Enrollment for ", input$courseSemester),
+             x = "Semester") + 
+        theme_economist() +
+        scale_color_economist() +
+        theme(axis.text.x = element_text(angle = 90)) 
+      ggplotly(plot1)
+    }
+    else if(!is.null(input$courseYearly) && input$courseYearly %in% course_reg_data_yearly$`Course Title`){
+        course_app_yearly <- course_reg_data_yearly |> filter(`Course Title` %in% input$courseYearly)
+        plot2 <-ggplot(data = course_app_yearly, aes(x = `Reporting Year`, y = Enrolled, group = 1)) +
+          geom_line() +
+          geom_point() +
+          labs(title = glue::glue("Enrollment for ", input$courseYearly)) + 
+          theme_economist() +
+          scale_color_economist() +
+          theme(axis.text.x = element_text(angle = 90))
+        ggplotly(plot2)
+    }
+    else if (!is.null(input$course) && 
+             (input$course) %in% course_reg_data$`Course Title`){
+      test1 <- course_reg_data_fall |> filter(`Course Title` %in% input$course)
+      test2 <- course_reg_data_spring |> filter(`Course Title` %in% input$course)
+      plot3 <- ggplot(data = course_reg_data, aes(x = `Reporting Year`, y = Enrolled, group = 1)) +
+        geom_line(data = test1, colour = "red") +
+        geom_line(data = test2) +
+        geom_point(data = test1, colour = "red") +
+        geom_point(data= test2) +
+        labs(title = glue::glue("Enrollment for ", input$course)) +
+        theme_economist() +
+        scale_color_economist() +
+        theme(axis.text.x = element_text(angle = 90))
+      ggplotly(plot3)
+    }
+      
   })
+  
 }
 
 shinyApp(ui, server)
+
