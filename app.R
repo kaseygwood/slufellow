f1aed92e27775b31185cf3d4edceb71897b3409e
Kasey Wood <kgwood20@stlawu.edu>
2023-06-12 19:22:11 -0400

    Fixed the issue. Changed the graph so the semesters are simply in order and not on different lines.

--- app.R
+++ app.R
@@ -12,60 +12,57 @@ ui <- fluidPage(
     tabPanel("Enrollment by Course",
              sidebarLayout(
                sidebarPanel(
-                 tabsetPanel(
-                   tabPanel("Yearly",
-                            selectInput("courseYearly", label = "Select a Course", 
-                                        choices = course_reg_data$course,
-                                        selected = course_reg_data$course[1])),
-                   tabPanel("By Semester",
-                            selectInput("courseSemester", label = "Select a Course", 
-                                        choices = course_reg_data$course, 
-                                        selected = course_reg_data$course[1]))
-                 )),
-               mainPanel(
-                 plotlyOutput(outputId = "plot")
-               )
-             )
+                   radioButtons("time", "Graph by year or by semester?",
+                                choices = c("Year", "Semester")),
+                   selectInput("courses", label = "Select a Course", 
+                                choices = course_reg_data$course,
+                                selected = course_reg_data$course[1])),
+               mainPanel(plotlyOutput(outputId = "plot"))
+             ))
     )
-  ))
+  )
 
 
 server <- function(input, output, session) {
   # plot enrollment by semester
   output$plot <- renderPlotly({
-    if ( input$courseSemester %in% course_reg_data$course){
-      course_app_semester <- course_reg_data |> filter(course %in% input$courseSemester) |> 
-        arrange(reporting_year)
-      plot1 <- ggplot(data = course_app_semester, aes(x = reporting_year, group = 1)) +
-        geom_line(aes(y = FA, colour = "Fall")) +
-        geom_point(aes(y = FA, colour = "Fall")) +
-        geom_line(aes(y = SP, colour = "Spring")) +
-        geom_point(aes(y = SP, colour = "Spring")) +
-        labs(title = glue::glue("Enrollment for ", input$courseSemester),
-             x = "Year",
-             y = "Enrollment",
-             colour = "Semester") + 
-        theme_economist() +
-        scale_color_economist() +
-        theme(axis.text.x = element_text(angle = 90)) 
-      ggplotly(plot1)
+    if ( input$courses %in% course_reg_data$course){
+      if (input$time == "Semester"){
+        course_app_semester <- course_reg_data |> 
+          pivot_longer(cols = c(FA, SP),names_to = "semester", values_to = "semester_enrolled") |> 
+          filter(course %in% input$courses) |> 
+          mutate(order = case_when(
+            semester == "SP" ~ as.numeric(reporting_year),
+            semester == "FA" ~ as.numeric(reporting_year) + 1)) |> 
+          unite("term", c(reporting_year, semester)) |>
+          mutate(term = fct_reorder(term, order, .desc = FALSE)) 
+        plot1 <- ggplot(data = course_app_semester, aes(x = term, y = semester_enrolled, group = 1)) +
+          geom_line() +
+          geom_point() +
+          labs(title = glue::glue("Enrollment for ", input$courses),
+               x = "Year",
+               y = "Enrollment",
+               colour = "Semester") + 
+          theme_economist() +
+          scale_color_economist() +
+          theme(axis.text.x = element_text(angle = 90)) 
+        ggplotly(plot1)
+      }
+      else if (input$time == "Year"){
+        course_app_yearly <- course_reg_data |> filter(course %in% input$courses)
+        plot2 <-ggplot(data = course_app_yearly, aes(x = reporting_year, y = yearly_enrolled, group = 1)) +
+          geom_line() +
+          geom_point() +
+          labs(title = glue::glue("Enrollment for ", input$courses),
+               x = "Year",
+               y = "Enrollment") + 
+          theme_economist() +
+          scale_color_economist() +
+          theme(axis.text.x = element_text(angle = 90))
+        ggplotly(plot2)
+      }
     }
-    else if( input$courseYearly %in% course_reg_data$course){
-      course_app_yearly <- course_reg_data |> filter(course %in% input$courseYearly)
-      plot2 <-ggplot(data = course_app_yearly, aes(x = reporting_year, y = yearly_enrolled, group = 1)) +
-        geom_line() +
-        geom_point() +
-        labs(title = glue::glue("Enrollment for ", input$courseYearly),
-             x = "Year",
-             y = "Enrollment") + 
-        theme_economist() +
-        scale_color_economist() +
-        theme(axis.text.x = element_text(angle = 90))
-      ggplotly(plot2)
-    }
-    
   })
-  
 }
 
 shinyApp(ui, server)
