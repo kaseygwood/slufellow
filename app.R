f223fea9202d41aa10a9afd7792def48367614e2
Kasey Wood <kgwood20@stlawu.edu>
2023-06-15 20:09:02 -0400

    edited the dataset to include sections, so that they could be included in the graphs. Added sections to the graphs in the app... need to edit the labels to properly display the data.

--- app.R
+++ app.R
@@ -4,7 +4,11 @@ library(readr)
 library(plotly)
 library(tidyverse)
 course_reg_data <- read_csv("course_reg_data.csv")
-
+course_reg_section <- course_reg_data |> 
+  pivot_longer((c(6:43)), names_to = "semester", values_to = "enrolled") |>
+  filter(enrolled != 0) 
+course_reg_section <- course_reg_section |> separate(col = semester, into = c("semester", "section"), sep = "_")
+course_reg_section <- course_reg_section |> unite("term", c(reporting_year, semester))
 
 ui <- fluidPage(
   theme = bslib::bs_theme(bootswatch = "sandstone"),
@@ -29,16 +33,26 @@ server <- function(input, output, session) {
     if ( input$courses %in% course_reg_data$course){
       if (input$time == "Semester"){
         course_app_semester <- course_reg_data |> 
+          rename("FA" = "fall_enrolled",
+                 "SP" = "spring_enrolled")|>
           pivot_longer(cols = c(FA, SP),names_to = "semester", values_to = "semester_enrolled") |> 
           filter(course %in% input$courses) |> 
           mutate(order = case_when(
-            semester == "SP" ~ as.numeric(reporting_year),
-            semester == "FA" ~ as.numeric(reporting_year) + 1)) |> 
+            semester == "SP" ~ reporting_year,
+            semester == "FA" ~ reporting_year + 1)) |> 
           unite("term", c(reporting_year, semester)) |>
           mutate(term = fct_reorder(term, order, .desc = FALSE)) 
-        plot1 <- ggplot(data = course_app_semester, aes(x = term, y = semester_enrolled, group = 1)) +
+        course_app_semester <- course_app_semester |>
+          filter(semester_enrolled != 0)
+        course_app_section <- course_reg_section |> 
+          filter(course %in% input$courses)|>
+          filter(enrolled != 0)
+        plot1 <- ggplot(data = course_app_semester, aes(x = term, y = semester_enrolled, label = semester_enrolled, group = 1)) +
           geom_line() +
           geom_point() +
+          scale_x_discrete(limits = levels(course_app_semester$term)) +
+          scale_y_continuous(limits = c(0, max(course_app_semester$semester_enrolled))) +
+          geom_point(data = course_app_section, aes(x = term, y = enrolled, label = section, group = 1), alpha = 0.5) +
           labs(title = glue::glue("Enrollment for ", input$courses),
                x = "Year",
                y = "Enrollment",
@@ -46,7 +60,7 @@ server <- function(input, output, session) {
           theme_economist() +
           scale_color_economist() +
           theme(axis.text.x = element_text(angle = 90)) 
-        ggplotly(plot1)
+        ggplotly(plot1, tooltip = c("label"))
       }
       else if (input$time == "Year"){
         course_app_yearly <- course_reg_data |> filter(course %in% input$courses)
--- departmental_data.R
+++ departmental_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
+
 course_reg_full <- read_excel("course_reg_14_23.xlsx")
 
 course_reg_data <- course_reg_full |> filter(Subject == "MATH" | 
@@ -9,34 +10,109 @@ course_reg_data <- course_reg_full |> filter(Subject == "MATH" |
   select(`Acad Year Term`, `Reporting Year`, `Course ID`, `Course Name`, 
          `Subject`, `Course Number`, `Course Title`, `Enrolled`, 
          `xlist Primary Course`, `xlist Section1`, `xlist Section2`, 
-         `xlist Section3`, `xlist Section4`) |> arrange(desc(`Reporting Year`))
-course_reg_data <- course_reg_data |> 
-  group_by(`Course Title`, `Acad Year Term`) |> 
-  summarize(Enrolled = sum(Enrolled),
-            `Reporting Year` = first(`Reporting Year`),
-            `Course Title` = first(`Course Title`)) |> 
-  rename("course" = "Course Title") |>
-  rename("term" = "Acad Year Term") |>
-  rename("reporting_year" = "Reporting Year") |>
-  filter(Enrolled >= 4) |> group_by(course, reporting_year) |>
-  arrange(reporting_year) |>
-  separate(col = term, into = c("year", "semester"), sep = -2) |>
-  pivot_wider(names_from = semester, values_from= Enrolled)
+         `xlist Section3`, `xlist Section4`, `Section Number`) |> arrange(desc(`Reporting Year`))
 
-course_reg_data <- course_reg_data |> select(-year) |> select(-SU)
+course_reg_data <- course_reg_data |> separate(col = `Acad Year Term`, into = c("reporting_year", "semester"), sep = -2) 
+course_reg_data <- course_reg_data |> unite("section",c(semester, `Section Number`))
+course_reg_data <- course_reg_data |> group_by(`Course Title`, `Reporting Year`) |> 
+  pivot_wider(names_from = section, values_from = Enrolled) |> 
+  select(-reporting_year) |>
+  rename("reporting_year" = "Reporting Year") |> rename("course" = "Course Title")
+course_reg_data <- course_reg_data |>
+  group_by(course, reporting_year) |>
+  summarize(FA_01 = sum(FA_01, na.rm = TRUE),
+            FA_02 = sum(FA_02, na.rm = TRUE),
+            FA_03 = sum(FA_03, na.rm = TRUE),
+            FA_04 = sum(FA_04, na.rm = TRUE),
+            FA_05 = sum(FA_05, na.rm = TRUE),
+            FA_06 = sum(FA_06, na.rm = TRUE),
+            FA_07 = sum(FA_07, na.rm = TRUE),
+            FA_08 = sum(FA_08, na.rm = TRUE),
+            FA_09 = sum(FA_09, na.rm = TRUE),
+            FA_10 = sum(FA_10, na.rm = TRUE),
+            SP_01 = sum(SP_01, na.rm = TRUE),
+            SP_02 = sum(SP_02, na.rm = TRUE),
+            SP_03 = sum(SP_03, na.rm = TRUE),
+            SP_04 = sum(SP_04, na.rm = TRUE),
+            SP_05 = sum(SP_05, na.rm = TRUE),
+            SP_06 = sum(SP_06, na.rm = TRUE),
+            SP_07 = sum(SP_07, na.rm = TRUE),
+            SP_08 = sum(SP_08, na.rm = TRUE),
+            SP_09 = sum(SP_09, na.rm = TRUE),
+            SP_11 = sum(SP_11, na.rm = TRUE),
+            SP_12 = sum(SP_12, na.rm = TRUE),
+            SP_13 = sum(SP_13, na.rm = TRUE),
+            SP_14 = sum(SP_14, na.rm = TRUE),
+            SP_15 = sum(SP_15, na.rm = TRUE),
+            SP_16 = sum(SP_16, na.rm = TRUE),
+            SP_18 = sum(SP_18, na.rm = TRUE),
+            SP_19 = sum(SP_19, na.rm = TRUE),
+            FA_11 = sum(FA_11, na.rm = TRUE),
+            FA_12 = sum(FA_12, na.rm = TRUE),
+            FA_13 = sum(FA_13, na.rm = TRUE),
+            FA_14 = sum(FA_14, na.rm = TRUE),
+            FA_15 = sum(FA_15, na.rm = TRUE),
+            FA_16 = sum(FA_16, na.rm = TRUE),
+            FA_17 = sum(FA_17, na.rm = TRUE),
+            FA_20 = sum(FA_20, na.rm = TRUE),
+            FA_21 = sum(FA_21, na.rm = TRUE),
+            FA_22 = sum(FA_22, na.rm = TRUE),
+            FA_23 = sum(FA_23, na.rm = TRUE))
 
-course_reg_data <- course_reg_data %>%
-  group_by(course, reporting_year) %>%
-  summarize(
-    reporting_year = first(reporting_year),
-    yearly_enrolled = sum(FA + SP, na.rm = TRUE),
-    FA = sum(FA, na.rm = TRUE),
-    SP = sum(SP, na.rm = TRUE)
-  )
-course_reg_data <- course_reg_data |> group_by(course, reporting_year) |>
-  summarize(yearly_enrolled = sum(FA + SP, na.rm = TRUE),
-            FA = first(FA),
-            SP = first(SP))
+View(course_reg_data)
+course_reg_data <- course_reg_data |>
+  group_by(course, reporting_year) |>
+  summarize(yearly_enrolled = sum(FA_01 + SP_01 + FA_02 + SP_02 + FA_03 + SP_03 +
+                                  FA_04 + SP_04 + FA_05 + SP_05 + FA_06 + SP_06 +
+                                    FA_07 + SP_07 + FA_08 + SP_08 + FA_09 + SP_09 + 
+                                    FA_10 + FA_11 + FA_12 + FA_13 + FA_14 + FA_15 +
+                                    FA_16 + FA_17 + FA_20 + FA_21 + FA_22 + FA_23 +
+                                    SP_11 + SP_12 + SP_13 + SP_14 + SP_15 + SP_16 +
+                                    SP_18 + SP_19, na.rm = TRUE),
+            fall_enrolled = sum(FA_01 + FA_02 + FA_03 + FA_04 + FA_05 + FA_06 + FA_07 + FA_08 + FA_09 + FA_10 + FA_11 + FA_12 + FA_13 + FA_14 + FA_15 +
+                                  FA_16 + FA_17 + FA_20 + FA_21 + FA_22 + FA_23, na.rm = TRUE),
+            spring_enrolled = sum(SP_01 + SP_02 + SP_03 + SP_04 + SP_05 + SP_06 + SP_07 + SP_08 + SP_09 + SP_11 + SP_12 + SP_13 + SP_14 + SP_15 + SP_16 +
+                                    SP_18 + SP_19, na.rm = TRUE),
+            FA_01 = first(FA_01),
+            FA_02 = first(FA_02),
+            FA_03 = first(FA_03),
+            FA_04 = first(FA_04),
+            FA_05 = first(FA_05),
+            FA_06 = first(FA_06),
+            FA_07 = first(FA_07),
+            FA_08 = first(FA_08),
+            FA_09 = first(FA_09),
+            FA_10 = first(FA_10),
+            SP_01 = first(SP_01),
+            SP_02 = first(SP_02),
+            SP_03 = first(SP_03),
+            SP_04 = first(SP_04),
+            SP_05 = first(SP_05),
+            SP_06 = first(SP_06),
+            SP_07 = first(SP_07),
+            SP_08 = first(SP_08),
+            SP_09 = first(SP_09),
+            SP_11 = first(SP_11),
+            SP_12 = first(SP_12),
+            SP_13 = first(SP_13),
+            SP_14 = first(SP_14),
+            SP_15 = first(SP_15),
+            SP_16 = first(SP_16),
+            SP_18 = first(SP_18),
+            SP_19 = first(SP_19),
+            FA_11 = first(FA_11),
+            FA_12 = first(FA_12),
+            FA_13 = first(FA_13),
+            FA_14 = first(FA_14),
+            FA_15 = first(FA_15),
+            FA_16 = first(FA_16),
+            FA_17 = first(FA_17),
+            FA_20 = first(FA_20),
+            FA_21 = first(FA_21),
+            FA_22 = first(FA_22),
+            FA_23 = first(FA_23))
+  
+View(course_reg_data)
 
 write_csv(course_reg_data, "course_reg_data.csv")
 
@@ -50,7 +126,7 @@ write_csv(course_reg_data, "course_reg_data.csv")
 #course_reg_data_spring <- course_reg_data |> separate(col = term, into = c("year", "semester"), sep = -2) |>
   #arrange(reporting_year) |> filter(semester == "SP")
 
-
+## TRY COMBINING SEMESTER AND SECTION BEFORESUMMINGTHESECTION
 
 
 
